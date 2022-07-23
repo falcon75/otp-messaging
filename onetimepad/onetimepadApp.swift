8712f6ddb7f23456b62775dbeeb57bb837b30a02
Sam McHale <sammchale2001@gmail.com>
2022-07-23 14:28:27 +0100

    Initial Commit

---
+++ onetimepad/onetimepadApp.swift
@@ -0,0 +1,17 @@
+//
+//  onetimepadApp.swift
+//  onetimepad
+//
+//  Created by Samuel McHale on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct onetimepadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
